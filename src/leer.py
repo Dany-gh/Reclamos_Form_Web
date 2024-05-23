2f5827892691102d354167b4f97fd684f392f30e
daniel <daguirreie@gmail.com>
2024-05-23 12:47:23 -0300

    Se ejecuta ok, leo 1 fila de hoja.

--- src/leer.py
+++ src/leer.py
@@ -3,9 +3,9 @@ from googleapiclient.discovery import build
 from google.oauth2 import service_account
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-KEY = 'key.json'
+KEY = 'clave_Reclamos_Form_Web.json'
 # Escribe aquí el ID de tu documento:
-SPREADSHEET_ID = ''
+SPREADSHEET_ID = '1FLWBfOe_ZKTMOviNBR35aC4CkHDGOwN2djyBd-rO0Js'
 
 creds = None
 creds = service_account.Credentials.from_service_account_file(KEY, scopes=SCOPES)
@@ -14,7 +14,7 @@ service = build('sheets', 'v4', credentials=creds)
 sheet = service.spreadsheets()
 
 # Llamada a la api
-result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range='Hoja 1!A1:A8').execute()
+result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range='RtaFormRecAgua!A1:I1').execute()
 # Extraemos values del resultado
 values = result.get('values',[])
 print(values)
