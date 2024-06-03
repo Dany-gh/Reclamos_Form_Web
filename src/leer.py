e6291f5a2a033c703f4ebae40e61a55524bb7416
daniel <daguirreie@gmail.com>
2024-06-03 13:21:54 -0300

    Depurando etapa final-Unico Word

--- src/leer.py
+++ src/leer.py
@@ -2,10 +2,19 @@
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
+#
 from pathlib import Path
 from googleapiclient.errors import HttpError
+#
 import os
+# Para word
+from docxtpl import DocxTemplate, InlineImage
+from docx.shared import Mm
+#
+import shutil
 
+# CONFIGURACION DE USUARIOS
+# ------- PARA EL GOOGLE SHEET
 # Alcances necesarios para acceder a la API de Google Sheets
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
@@ -13,6 +22,21 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 # ID o URL de la hoja de cálculo y el Rango
 SPREADSHEET_ID = '1FLWBfOe_ZKTMOviNBR35aC4CkHDGOwN2djyBd-rO0Js'
 SHEET_NAME = 'ReclamosRes055-20'
+
+# -------- PARA EL WORD
+# Ruta de Salida
+OUTPUT_PATH= '.\Outputs'
+# Ruta al fichero Excel
+EXCEL_PATH= '.\Inputs\People_Data.xlsx'
+
+# Ruta plantillas Ficheros word
+ES_WORD_TPL_PATH='.\Inputs\Templates\WordTemplate_ES.docx'
+EN_WORD_TPL_PATH='.\Inputs\Templates\WordTemplate_EN.docx'
+WORD_TPL_PRUEBA='.\Inputs\Templates\TemplateRECLAMOS_LUZ.docx'
+
+# Ruta de Imagenes
+IMAGE_PATH='.\Inputs\Images'
+
 
 #==============================================================================================================================
 def clear_screen():
@@ -74,6 +98,91 @@ def print_row_data(row):
     print('-' * 80)  # Línea separadora entre filas
 #------------------------------------------------------------------------------------------------------------------------------
 
+# Rutina para eliminar y crear carpeta
+def EliminarCrearCarpetas(path):
+    #Verificar si la carpeta existe y elimninarla
+    if(os.path.exists(path)):
+        shutil.rmtree(path)
+        
+    # Crear carpeta
+    os.mkdir(OUTPUT_PATH)
+
+#==============================================================================================================================
+# Rutina para crear un fichero word para cada persona 
+def CrearWordPersonas(df_pers):
+    # Iteramos sobre cada Persona
+    for r_idx, r_val in enumerate(df_pers):
+        # Cargar plantilla
+        l_tpl=ES_WORD_TPL_PATH
+        '''
+        if (r_val['Idioma'] == 'ES'):
+            l_tpl=ES_WORD_TPL_PATH
+        elif (r_val['Idioma'] == 'EN'):
+            l_tpl=EN_WORD_TPL_PATH
+        '''
+        # Procesamos la plantilla
+        docx_tpl=DocxTemplate(l_tpl)
+
+        # Añadir imagen grafico circular y de barra
+        #img_path = IMAGE_PATH + '\\' + r_val['Imagen']
+        #img = InlineImage(docx_tpl, img_path, height=Mm(15))
+
+        # Crear contexto
+        # word : Google Sheet
+        
+        context = {
+            'name': r_val['Nombre'],
+            'surname1': r_val['Apellido'],
+            #'surname2': r_val['Telefono de Contacto'], # En este me da error
+            'edad': r_val['Correo Electrónico'],
+            #'picture': img,
+        }
+        # Crear el contexto (ChatGPT)
+        #contexto = {'items': [{'indice': i, 'valor': v} for i, v in enumerate(df_pers)]}
+
+        # Verificar el contexto
+        print("Contexto:", context)
+
+        # Renderizamos usando el contexto creado
+        docx_tpl.render(context)
+
+        # Guardamos el documento
+        nombre_doc = 'Documento_' + r_val['Apellido'].upper() + '_' + r_val['Nombre'] + '.docx'
+        '''
+        if(pd.notna(r_val['Apellido2'])):
+            nombre_doc = 'Documento_' + r_val['Apellido1'].upper() + '_' + r_val['Apellido2'].upper() + '_' + r_val['Nombre'] + '.docx'
+        else:
+            nombre_doc = 'Documento_' + r_val['Apellido1'].upper() + '_' + r_val['Nombre'] + '.docx'
+        docx_tpl.save(OUTPUT_PATH + '\\' + nombre_doc)
+        '''
+        docx_tpl.save(OUTPUT_PATH + '\\' + nombre_doc)
+#==============================================================================================================================
+# Rutina para crear un fichero word para TODAS las personas 
+def crea_documento_unico(datos):
+    try:
+        # Convertir la lista a una lista de diccionarios
+        #lista_diccionarios = [{"nombre": item[0], "edad": item[1]} for item in datos]
+        
+        # Cargar la plantilla
+        doc = DocxTemplate(WORD_TPL_PRUEBA)
+        
+        # Crear el contexto con todos los datos
+        contexto = {'items': datos}
+        
+        # Verificar el contexto
+        print("Contexto:", contexto)
+        
+        # Renderizar el documento con el contexto
+        doc.render(contexto)
+        
+        nombre_doc = 'Reclamos_' + '.docx'
+        # Guardar el documento
+        doc.save(OUTPUT_PATH + '\\' + nombre_doc)
+        
+        print(f"Documento guardado como {nombre_doc}")
+    
+    except Exception as e:
+        print("Ocurrió un error:", e)
 
 def main():
     # Ruta al archivo de credenciales JSON
@@ -91,10 +200,10 @@ def main():
 
         # Obtén el rango de datos existente en la hoja, por medio de la columna A
         result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=f'{SHEET_NAME}!A:A').execute()
-        print("\033[34m Resul:\033[0m") #Imprime en color rojo
+        print("\033[34m Resul:\033[0m") #Imprime en color azul
         print(result) # Imprime la lista
         rows = result.get('values', []) # Aqui solo son las filas de la primera columna (A)
-        print(f"\033[34m {len(rows)} : rows Recuperadas.\033[0m")
+        print(f"\033[34m {len(rows)} : Filas (rows) Recuperadas.\033[0m")
         # Saco la cantidad de filas que tienen datos, per no sabemos si estan pintadas
         num_rows = len(result.get('values', [])) - 1 # Descuento la cabecera
             
@@ -169,20 +278,27 @@ def main():
                 range_to_read = f'{SHEET_NAME}!A{first_unread_row}:F{last_unread_row}'  # Ajusta el rango según sea necesario
                 record = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=range_to_read).execute()
                 print("\033[34m RECORD: \033[0m")
-                datos = record.get('values',[])
+                datos_lista = record.get('values',[])
                 # La primera fila contiene los nombres de las columnas
                 #columns = datos[0][1]
                 #data = datos[1:]
-                if not datos:
+                if not datos_lista:
                     print('No data found.')
                 else:
                     # Itera sobre las filas y las imprime
-                    for row in datos:
+                    for row in datos_lista:
                         print_row_data(row)
 
+                # Convertir la lista en una lista de diccionarios
+                datos_diccionario = [{"Marca temporal": item[0], 
+                                      "Apellido": item[1], 
+                                      "Nombre" : item[2], 
+                                      "Telefono de Contacto" : item[3],
+                                      "Correo Electrónico" : item[4],
+                                      "Nro de Factura" : item[5]} for item in datos_lista]
 
                 # Convierte los datos a un DataFrame de Pandas
-                if not datos:
+                if not datos_lista:
                     print('No data found.')
                 else:
                     # Asume que la primera fila de values contiene los nombres de las columnas
@@ -190,6 +306,10 @@ def main():
 
                     # Muestra el DataFrame
                     print()
+                
+                EliminarCrearCarpetas(OUTPUT_PATH)
+                #CrearWordPersonas(datos_diccionario)
+                crea_documento_unico(datos_diccionario)
 
                 #--chatGPT-------------------------------------------------------------------------------
                 # Obtiene los detalles de la hoja de cálculo. Saco el sheet_id
@@ -209,7 +329,7 @@ def main():
                 requests = [{
                     'updateCells': {
                         'range': {
-                            'sheetId': sheet_id,
+                            'sheetId': sheet_id,    # Id de la hoja con la cual estamos trabajando
                             'startRowIndex': star_Fila, # Inicio del rango de filas.
                             'endRowIndex': end_Fila,   # Para abarcar mas filas (es exclusive)
                             'startColumnIndex': 0,
